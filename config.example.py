f551f3044a3675fadd0c5f4a89fb6cb000d0505d
FerrumVega <ferrumthevega@gmail.com>
2025-08-19 18:09:21 +0300

    first commit

---
+++ config.example.py
@@ -0,0 +1,3 @@
+API_ID = 12345678  # App api_id с https://my.telegram.org/apps
+API_HASH = "API_HASH"  # App api_hash с https://my.telegram.org/apps
+CHECK_DELAY = 1  # Задержка в секундах между проверками на новые подарки
